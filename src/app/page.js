e4f00fd34291958f75e4e789ed078c8f57431edd
Kate <kteagles21@gmail.com>
2024-09-12 21:18:27 -0400

    adding about erin section

--- src/app/page.js
+++ src/app/page.js
@@ -6,22 +6,22 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-white">
       <main>
-        <section className="relative h-screen bg-cover bg-center flex items-start" style={{backgroundImage: "url('/couch.png')"}}>
+        <section className="relative h-[80vh] bg-cover bg-center flex items-start" style={{backgroundImage: "url('/couch.png')"}}>
           <div className="absolute inset-0 bg-black bg-opacity-30"></div>
-          <div className="relative w-full flex flex-col items-center text-white text-center p-4 pt-40 font-serif">
-            <h1 className="text-6xl font-light mb-4 font-sacramento">Erin Eagles LCSW</h1> 
-            <p className="text-3xl mb-4">Licensed Clinical Social Worker</p> 
-            <p className="text-3xl italic">Your mental health is a priority. Your happiness is essential. Your self-care is a necessity.</p>
-            <p className="mt-2 text-2xl italic">— Unknown</p> 
+          <div className="relative w-full flex flex-col items-center justify-center text-white text-center p-4 pt-40 font-serif">
+            <h1 className="text-8xl font-light mb-4 font-sacramento">Erin Eagles LCSW</h1> 
+            <p className="text-4xl mb-4">Licensed Clinical Social Worker</p> 
+            <p className="text-4xl italic">Your mental health is a priority. Your happiness is essential. Your self-care is a necessity.</p>
+            <p className="mt-2 text-xl italic">— Unknown</p> 
           </div>
         </section>
 
         <section id="services" className="py-24 px-8 max-w-6xl mx-auto">
-          <h2 className="text-4xl text-gray-600 mb-12 font-sacramento">My Services</h2>
-          <div className="flex flex-col text-gray-600 md:flex-row gap-12">
+          <h2 className="text-5xl text-gray-600 mb-12 font-sacramento">My Services</h2>
+          <div className="flex flex-col text-gray-600 md:flex-row gap-12 items-start">
             <div className="md:w-3/5">
               <p className="text-2xl mb-6">At Erin Eagles LCSW, we offer a range of therapy services tailored to meet your individual needs. Our approach is client-centered, focusing on creating a safe and supportive environment for personal growth and healing.</p>
-              <p className="text-4xl mb-4 font-sacramento">My services include:</p>
+              <p className="text-5xl mb-4 font-sacramento">My services include:</p>
               <ul className="list-disc list-inside mt-4 text-2xl space-y-2">
                 <li>Individual Therapy</li>
                 <li>Family Therapy</li>
@@ -30,8 +30,21 @@ export default function Home() {
                 <li>Ketamine-Assisted Psychotherapy (KAP)</li>
               </ul>
             </div>
-            <div className="md:w-2/5">
-              <Image src="/conform.png" alt="Therapy Session" width={500} height={600} className="rounded-lg" />
+            <div className="md:w-1/2 md:-mt-16">
+              <Image src="/conform.png" alt="Therapy Session" width={600} height={200} className="rounded-lg" />
+            </div>
+          </div>
+        </section>
+
+        <section id="about" className="py-24 px-8 max-w-6xl mx-auto">
+          <h2 className="text-5xl text-gray-600 mb-12 font-sacramento text-center mx-auto">Who I am</h2>
+          <div className="flex flex-col-reverse text-gray-600 md:flex-row gap-12 items-center">
+            <div className="md:w-1/2">
+              <Image src="/erin.png" alt="Erin Eagles LCSW" width={600} height={200} className="rounded-lg" />
+            </div>
+            <div className="md:w-1/2 flex flex-col justify-center">
+              <p className="text-2xl mb-6">I am a Licensed Clinical Social Worker (LCSW) dedicated to providing compassionate and comprehensive mental health care. With expertise in eating disorders, LGBTQ+ issues, depression, anxiety, and trauma, I offer a safe and supportive environment for clients to explore their challenges and achieve personal growth. My client-centered approach combines empathy with clinical expertise, including specialized training in Ketamine-Assisted Psychotherapy (KAP). Whether you're struggling with long-standing issues or facing new life challenges, I am committed to empowering you on your journey towards healing and self-discovery, helping you build resilience and lead a more fulfilling life.</p>
+              <p className="text-4xl mb-4 font-sacramento">- Erin Eagles</p>
             </div>
           </div>
         </section>
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -5,12 +5,12 @@ import Link from 'next/link';
 const Menu = () => {
   return (
     <nav className="bg-white shadow-md p-4 fixed w-full z-10">
-      <ul className="flex flex-wrap justify-center space-x-8">
-          <li><Link href="/" className="text-2xl text-gray-600 hover:text-indigo-600">Home</Link></li>
-          <li><Link href="/#services" className="text-2xl text-gray-600 hover:text-indigo-600">Services</Link></li>
-          <li><Link href="/#contact" className="text-2xl text-gray-600 hover:text-indigo-600">Contact</Link></li>
-          <li><Link href="/#about-me" className="text-2xl text-gray-600 hover:text-indigo-600">About Me</Link></li>
-          <li><Link href="/#kap-treatment" className="text-2xl text-gray-600 hover:text-indigo-600">KAP Treatment</Link></li>
+        <ul className="flex flex-wrap justify-center space-x-8">
+          <li><Link href="/" className="text-xl text-gray-600 hover:text-indigo-600">Home</Link></li>
+          <li><Link href="/#services" className="text-xl text-gray-600 hover:text-indigo-600">Services</Link></li>
+          <li><Link href="/#contact" className="text-xl text-gray-600 hover:text-indigo-600">Contact</Link></li>
+          <li><Link href="/#about-me" className="text-xl text-gray-600 hover:text-indigo-600">About Me</Link></li>
+          <li><Link href="/#kap-treatment" className="text-xl text-gray-600 hover:text-indigo-600">KAP Treatment</Link></li>
         </ul>
         </nav>
        
